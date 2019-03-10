06358299467fdf810bbc6be57f0a88e471d7aad7
Utkan Sargin <utkan.sargin@bonial.com>
2019-03-10 14:56:31 +0100

    Add Characters view model.

---
+++ App/presentation/src/main/java/io/utkan/marvel/presentation/CharactersViewModel.kt
@@ -0,0 +1,40 @@
+package io.utkan.marvel.presentation
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.utkan.marvel.domain.interactor.GetCharacters
+import io.utkan.marvel.domain.model.CharacterDomain
+import javax.inject.Inject
+
+class CharactersViewModel @Inject constructor(
+    getCharacters: GetCharacters
+) : ViewModel() {
+
+    private val _viewState = MutableLiveData<ViewState>()
+    private var state: ViewState?
+        get() = _viewState.value
+        set(value) {
+            if (_viewState.value != value) {
+                _viewState.value = value
+            }
+        }
+    val viewState: LiveData<ViewState> = _viewState
+
+    init {
+        state = ViewState.Loading
+        getCharacters.execute(
+            {throwable->
+                _viewState.postValue(ViewState.Error(throwable.localizedMessage))
+            },{results->
+//                state = ViewState.CharacterList(results)
+                _viewState.postValue(ViewState.CharacterList(results))
+            }
+        )
+    }
+
+    sealed class ViewState {
+        object Loading : ViewState()
+        data class Error(val message: String?) : ViewState()
+        data class CharacterList(val characters: List<CharacterDomain>) : ViewState()
+    }
+}
